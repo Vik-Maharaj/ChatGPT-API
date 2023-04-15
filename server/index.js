794046e31747479fbe30db66c87a9f5d83033860
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-04-14 21:54:05 -0400

    Add openai variable

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
+app.use(express.json());
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}`));
