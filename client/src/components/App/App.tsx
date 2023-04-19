f81deab2a4359c3b12594038a27541715a55d2a0
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-04-19 14:52:29 -0400

    Update const app with setprompt

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -3,6 +3,20 @@ import axios from "axios";
 
 const App = () => {
 
+    const [prompt, setPrompt] = useState<string>('');
+
+      const appLoader = (uid: string) => {
+        const element = document.getElementById(uid) as HTMLElement;
+        element.textContent = ''
+    
+        loadInterval = setInterval(() => {
+          element.textContent += '.';
+    
+          if (element.textContent === '....') {
+            element.textContent = '';
+          }
+        }, 300);
+      }
 }
 
 export default App;
